c6b9eb60498c59b9648d55a7e08596ddb9ab1723
Wei Yang <davidyoung8906@gmail.com>
2015-10-11 21:27:33 -0500

    rest of data

--- js/blog.js
+++ js/blog.js
@@ -15,6 +15,22 @@ $(function() {
 	// });
     var blogs = new Blogs();
 
+    var DateFormats = {
+       short: "DD MMMM - YYYY",
+       long: "dddd DD.MM.YYYY HH:mm"
+	};
+
+	Handlebars.registerHelper("formatDate", function(datetime, format) {
+  		if (moment) {
+    	// can use other formats like 'lll' too
+    		format = DateFormats[format] || format;
+    		return moment(datetime).format(format);
+ 		}
+  		else {
+    		return datetime;
+  		}
+	});
+
  	var BlogsView =  Parse.View.extend({
     	template: Handlebars.compile($('#blogs-tpl').html()),
     	render: function(){ 
